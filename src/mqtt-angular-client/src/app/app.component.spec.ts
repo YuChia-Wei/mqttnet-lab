179bc31e53e57121d5493c6aba63fc174bf24438
YuChia <h42279@gmail.com>
2022-12-31 23:32:26 +0800

    fix angular project namming

--- src/mqtt-angular-client/src/app/app.component.spec.ts
+++ src/mqtt-angular-client/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mqtt-anguler-client'`, () => {
+  it(`should have as title 'mqtt-angular-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mqtt-anguler-client');
+    expect(app.title).toEqual('mqtt-angular-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mqtt-anguler-client app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mqtt-angular-client app is running!');
   });
 });
