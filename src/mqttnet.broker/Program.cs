fb04c7c694b99aed6b12dfd35b30f09bd81f0336
YuChia <h42279@gmail.com>
2022-12-31 23:03:30 +0800

    設定初版 mqtt broker

--- src/mqttnet.broker/Program.cs
+++ src/mqttnet.broker/Program.cs
@@ -1,6 +1,60 @@
+using MQTTnet.AspNetCore;
+using MQTTnet.Server;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // link = mqtt://{host}:1883
+    options.ListenAnyIP(1883, listenOptions => listenOptions.UseMqtt());
+    // UseMqtt 實際上是下面這行
+    // options.ListenAnyIP(1883, listenOptions =>listenOptions.UseConnectionHandler<MqttConnectionHandler>());
+
+    // link = wss://{host}:64430
+    options.ListenAnyIP(64430, listenOptions => listenOptions.UseHttps());
+
+    // link = ws://{host}:4430
+    options.ListenAnyIP(4430);
+});
+
+builder.Services.AddMqttServer();
+// AddMqttServer = 以下兩行
+// builder.Services.AddMqttConnectionHandler();
+// builder.Services.AddHostedMqttServer();
+
+builder.Services.AddConnections();
+
+builder.Services.AddSingleton<MqttEvents>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+//ws / wss route
+app.MapMqtt("/mqtt");
+
+//default route = mqtt://{host}:1883
+app.UseMqttServer(server =>
+{
+    var requiredService = app.Services.GetRequiredService<MqttEvents>();
+
+    server.ClientConnectedAsync += requiredService.OnClientConnected;
+    server.ValidatingConnectionAsync += requiredService.ValidateConnection;
+});
 
 app.Run();
+
+internal sealed class MqttEvents
+{
+    public Task OnClientConnected(ClientConnectedEventArgs eventArgs)
+    {
+        Console.WriteLine($"Client '{eventArgs.ClientId}' connected.");
+        return Task.CompletedTask;
+    }
+
+    public Task ValidateConnection(ValidatingConnectionEventArgs eventArgs)
+    {
+        Console.WriteLine($"Client '{eventArgs.ClientId}' wants to connect. Accepting!");
+        return Task.CompletedTask;
+    }
+}
