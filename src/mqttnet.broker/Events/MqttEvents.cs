3fd7e6808783713ed71bf839c4743240f8c739d8
YuChia <h42279@gmail.com>
2023-01-02 23:42:08 +0800

    更新命名空間

---
+++ src/mqttnet.broker/Events/MqttEvents.cs
@@ -0,0 +1,31 @@
+﻿using MQTTnet.Server;
+
+namespace mqttnet.broker.Events;
+
+internal sealed class MqttEvents
+{
+    public Task OnClientConnected(ClientConnectedEventArgs eventArgs)
+    {
+        Console.WriteLine($"Client '{eventArgs.ClientId}' connected.");
+        return Task.CompletedTask;
+    }
+
+    public Task OnClientDisconnected(ClientDisconnectedEventArgs eventArgs)
+    {
+        Console.WriteLine($"Client '{eventArgs.ClientId}' disconnected.");
+        return Task.CompletedTask;
+    }
+
+    public Task OnInterceptingPublish(InterceptingPublishEventArgs eventArgs)
+    {
+        Console.WriteLine(
+            $"Client '{eventArgs.ClientId}' publish: {eventArgs.ApplicationMessage.Topic}:{eventArgs.ApplicationMessage.Payload}");
+        return Task.CompletedTask;
+    }
+
+    public Task ValidateConnection(ValidatingConnectionEventArgs eventArgs)
+    {
+        Console.WriteLine($"Client '{eventArgs.ClientId}' wants to connect. Accepting!");
+        return Task.CompletedTask;
+    }
+}
--- src/mqttnet.broker/Program.cs
+++ src/mqttnet.broker/Program.cs
@@ -1,5 +1,5 @@
 using MQTTnet.AspNetCore;
-using MQTTnet.Server;
+using mqttnet.broker.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -45,31 +45,3 @@ app.UseMqttServer(server =>
 });
 
 app.Run();
-
-internal sealed class MqttEvents
-{
-    public Task OnClientConnected(ClientConnectedEventArgs eventArgs)
-    {
-        Console.WriteLine($"Client '{eventArgs.ClientId}' connected.");
-        return Task.CompletedTask;
-    }
-
-    public Task OnClientDisconnected(ClientDisconnectedEventArgs eventArgs)
-    {
-        Console.WriteLine($"Client '{eventArgs.ClientId}' disconnected.");
-        return Task.CompletedTask;
-    }
-
-    public Task OnInterceptingPublish(InterceptingPublishEventArgs eventArgs)
-    {
-        Console.WriteLine(
-            $"Client '{eventArgs.ClientId}' publish: {eventArgs.ApplicationMessage.Topic}:{eventArgs.ApplicationMessage.Payload}");
-        return Task.CompletedTask;
-    }
-
-    public Task ValidateConnection(ValidatingConnectionEventArgs eventArgs)
-    {
-        Console.WriteLine($"Client '{eventArgs.ClientId}' wants to connect. Accepting!");
-        return Task.CompletedTask;
-    }
-}
