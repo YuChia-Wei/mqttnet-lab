2f9fccf2066346649482db28057e0ede57ee3927
YuChia <h42279@gmail.com>
2023-01-02 16:12:22 +0800

    publish: 加入基本的發布 API

--- src/mqttnet.publish/Program.cs
+++ src/mqttnet.publish/Program.cs
@@ -1,6 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using MQTTnet;
+using MQTTnet.Client;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IMqttClient>(_ => new MqttFactory().CreateMqttClient());
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapPut("/publish", async (
+               [FromServices] IMqttClient mqttClient,
+               [FromBody] MqttData mqttData,
+               CancellationToken stoppingToken) =>
+           {
+               var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("localhost").Build();
+               await mqttClient.ConnectAsync(mqttClientOptions, stoppingToken);
+
+               var applicationMessage = new MqttApplicationMessageBuilder()
+                                        .WithTopic(mqttData.TopicId)
+                                        .WithPayload(mqttData.Data)
+                                        .Build();
+
+               await mqttClient.PublishAsync(applicationMessage, stoppingToken);
+
+               await mqttClient.DisconnectAsync();
+           });
 
 app.Run();
+
+public record MqttData
+{
+    public string Data { get; set; }
+    public string TopicId { get; set; }
+}
