a3a892a94d6ef513ab63f898e3371b159b8c265e
YuChia <h42279@gmail.com>
2023-01-10 22:44:53 +0800

    補上其他 Server 端事件

--- src/MQTTnet.AspNetCore.Server/Extensions/ApplicationBuilderExtensions.cs
+++ src/MQTTnet.AspNetCore.Server/Extensions/ApplicationBuilderExtensions.cs
@@ -17,9 +17,7 @@ public static class ApplicationBuilderExtensions
         Func<ApplicationMessageNotConsumedEventArgs, Task> eventFunc)
     {
         var server = app.ApplicationServices.GetRequiredService<MqttServer>();
-
         server.ApplicationMessageNotConsumedAsync += eventFunc;
-
         return app;
     }
 
@@ -34,9 +32,7 @@ public static class ApplicationBuilderExtensions
         Func<ClientAcknowledgedPublishPacketEventArgs, Task> eventFunc)
     {
         var server = app.ApplicationServices.GetRequiredService<MqttServer>();
-
         server.ClientAcknowledgedPublishPacketAsync += eventFunc;
-
         return app;
     }
 
@@ -51,9 +47,7 @@ public static class ApplicationBuilderExtensions
         Func<ClientConnectedEventArgs, Task> eventFunc)
     {
         var server = app.ApplicationServices.GetRequiredService<MqttServer>();
-
         server.ClientConnectedAsync += eventFunc;
-
         return app;
     }
 
@@ -68,9 +62,37 @@ public static class ApplicationBuilderExtensions
         Func<ClientDisconnectedEventArgs, Task> eventFunc)
     {
         var server = app.ApplicationServices.GetRequiredService<MqttServer>();
-
         server.ClientDisconnectedAsync += eventFunc;
+        return app;
+    }
 
+    /// <summary>
+    /// Append Client Subscribed Topic Event
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="eventFunc"></param>
+    /// <returns></returns>
+    public static IApplicationBuilder UseClientSubscribedTopicEvent(
+        this IApplicationBuilder app,
+        Func<ClientSubscribedTopicEventArgs, Task> eventFunc)
+    {
+        var server = app.ApplicationServices.GetRequiredService<MqttServer>();
+        server.ClientSubscribedTopicAsync += eventFunc;
+        return app;
+    }
+
+    /// <summary>
+    /// Append Client Unsubscribed Topic Event
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="eventFunc"></param>
+    /// <returns></returns>
+    public static IApplicationBuilder UseClientUnsubscribedTopicEvent(
+        this IApplicationBuilder app,
+        Func<ClientUnsubscribedTopicEventArgs, Task> eventFunc)
+    {
+        var server = app.ApplicationServices.GetRequiredService<MqttServer>();
+        server.ClientUnsubscribedTopicAsync += eventFunc;
         return app;
     }
 
@@ -85,9 +107,7 @@ public static class ApplicationBuilderExtensions
         Func<InterceptingPacketEventArgs, Task> eventFunc)
     {
         var server = app.ApplicationServices.GetRequiredService<MqttServer>();
-
         server.InterceptingInboundPacketAsync += eventFunc;
-
         return app;
     }
 
@@ -102,9 +122,7 @@ public static class ApplicationBuilderExtensions
         Func<InterceptingPacketEventArgs, Task> eventFunc)
     {
         var server = app.ApplicationServices.GetRequiredService<MqttServer>();
-
         server.InterceptingOutboundPacketAsync += eventFunc;
-
         return app;
     }
 
@@ -119,9 +137,52 @@ public static class ApplicationBuilderExtensions
         Func<InterceptingPublishEventArgs, Task> eventFunc)
     {
         var server = app.ApplicationServices.GetRequiredService<MqttServer>();
-
         server.InterceptingPublishAsync += eventFunc;
+        return app;
+    }
 
+    /// <summary>
+    /// Append Intercepting Subscription Event
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="eventFunc"></param>
+    /// <returns></returns>
+    public static IApplicationBuilder UseInterceptingSubscriptionEvent(
+        this IApplicationBuilder app,
+        Func<InterceptingSubscriptionEventArgs, Task> eventFunc)
+    {
+        var server = app.ApplicationServices.GetRequiredService<MqttServer>();
+        server.InterceptingSubscriptionAsync += eventFunc;
+        return app;
+    }
+
+    /// <summary>
+    /// Append Intercepting Unsubscription Event
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="eventFunc"></param>
+    /// <returns></returns>
+    public static IApplicationBuilder UseInterceptingUnsubscriptionEvent(
+        this IApplicationBuilder app,
+        Func<InterceptingUnsubscriptionEventArgs, Task> eventFunc)
+    {
+        var server = app.ApplicationServices.GetRequiredService<MqttServer>();
+        server.InterceptingUnsubscriptionAsync += eventFunc;
+        return app;
+    }
+
+    /// <summary>
+    /// Append Loading Retained Messages Event
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="eventFunc"></param>
+    /// <returns></returns>
+    public static IApplicationBuilder UseLoadingRetainedMessagesEvent(
+        this IApplicationBuilder app,
+        Func<LoadingRetainedMessagesEventArgs, Task> eventFunc)
+    {
+        var server = app.ApplicationServices.GetRequiredService<MqttServer>();
+        server.LoadingRetainedMessageAsync += eventFunc;
         return app;
     }
 
@@ -136,9 +197,84 @@ public static class ApplicationBuilderExtensions
         Func<ValidatingConnectionEventArgs, Task> eventFunc)
     {
         var server = app.ApplicationServices.GetRequiredService<MqttServer>();
-
         server.ValidatingConnectionAsync += eventFunc;
+        return app;
+    }
 
+    /// <summary>
+    /// Append Preparing Session Event (maybe error?)
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="eventFunc"></param>
+    /// <returns></returns>
+    internal static IApplicationBuilder UsePreparingSessionEvent(
+        this IApplicationBuilder app,
+        Func<PreparingSessionEventArgs, Task> eventFunc)
+    {
+        //TODO: 官方套件的 PreparingSessionAsync 並不是接 PreparingSessionEventArgs，而是 EventArgs
+        // var server = app.ApplicationServices.GetRequiredService<MqttServer>();
+        // server.PreparingSessionAsync += eventFunc;
+        return app;
+    }
+
+    /// <summary>
+    /// Append Preparing Session Event (maybe error?)
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="eventFunc"></param>
+    /// <returns></returns>
+    internal static IApplicationBuilder UsePreparingSessionEvent(
+        this IApplicationBuilder app,
+        Func<EventArgs, Task> eventFunc)
+    {
+        //TODO: 官方套件的 PreparingSessionAsync 並不是接 PreparingSessionEventArgs，而是 EventArgs
+        var server = app.ApplicationServices.GetRequiredService<MqttServer>();
+        server.PreparingSessionAsync += eventFunc;
+        return app;
+    }
+
+    /// <summary>
+    /// Append Retained Message Changed Event
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="eventFunc"></param>
+    /// <returns></returns>
+    internal static IApplicationBuilder UseRetainedMessageChangedEvent(
+        this IApplicationBuilder app,
+        Func<RetainedMessageChangedEventArgs, Task> eventFunc)
+    {
+        var server = app.ApplicationServices.GetRequiredService<MqttServer>();
+        server.RetainedMessageChangedAsync += eventFunc;
+        return app;
+    }
+
+    /// <summary>
+    /// Append Retained Message Cleared Event
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="eventFunc"></param>
+    /// <returns></returns>
+    internal static IApplicationBuilder UseRetainedMessagesClearedEvent(
+        this IApplicationBuilder app,
+        Func<EventArgs, Task> eventFunc)
+    {
+        var server = app.ApplicationServices.GetRequiredService<MqttServer>();
+        server.RetainedMessagesClearedAsync += eventFunc;
+        return app;
+    }
+
+    /// <summary>
+    /// Append Session Deleted Event
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="eventFunc"></param>
+    /// <returns></returns>
+    internal static IApplicationBuilder UseSessionDeletedEvent(
+        this IApplicationBuilder app,
+        Func<SessionDeletedEventArgs, Task> eventFunc)
+    {
+        var server = app.ApplicationServices.GetRequiredService<MqttServer>();
+        server.SessionDeletedAsync += eventFunc;
         return app;
     }
 }
